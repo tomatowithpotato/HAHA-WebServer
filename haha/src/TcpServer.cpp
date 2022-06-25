7f02530f06d3ecb73e0a34dc82fc4b3f37eeafc9
tomatowithpotato <1107682451@qq.com>
2022-06-25 15:57:37 +0800

    fix sigpipe bug

--- haha/src/TcpServer.cpp
+++ haha/src/TcpServer.cpp
@@ -15,7 +15,11 @@ TcpServer::TcpServer()
 }
 
 void TcpServer::start(const InetAddress &address){
-    ::signal(SIGPIPE, SIG_IGN);
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGPIPE);
+    pthread_sigmask(SIG_BLOCK, &set, NULL);
+
     servSock_.bind(address);
     servSock_.listen();
     listenChannel_ = std::make_shared<Channel>(eventLoop_.get(), servSock_.getFd(), false);
